5d98bacdf21a376e132bee8eccd0394b9ad1d0dd
sttraping <152054372+sttraping@users.noreply.github.com>
2026-06-18 13:00:57 +0200

    Refactor test_get_nth_fibonacci for clarity

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -53,18 +53,17 @@ def test_get_nth_fibonacci_one():
 
     # Act
     result = get_nth_fibonacci(n)
-
     # Assert
     assert result == 1
 
 
 def test_get_nth_fibonacci_ten():
  """Test with n=10."""
-#     # Arrange
-n = 10
+ # Arrange
+ n = 10
 
-# Act
+ # Act
  result = get_nth_fibonacci(n)
 
  # Assert
-assert result == 89
+ assert result == 89
